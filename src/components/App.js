87bb04baf7d9a63b7e9ca266460b85454f6efb23
Cosmo Kent <cosmolightfoot@gmail.com>
2019-05-20 11:27:20 -0700

    BRY components made, working on header

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  BrowserRouter as Router
+} from 'react-router-dom';
+import Header from './Header.js'
+import Yellow from './Yellow.js';
+import Blue from './Blue.js';
+import Red from './Red.js';
+
+export default function App() {
+  return (
+    <Router>
+
+    </Router>
+      <Yellow />
+  );
+}
